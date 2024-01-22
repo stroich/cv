9325df69bcca70e1f996992f05c3a5c2d647795f
stroich <stroichmwork@gmail.com>
2024-01-22 23:23:13 +0400

    feat: add languages

---
+++ data.js
@@ -0,0 +1,75 @@
+export const languageResources = {
+    'en': {
+        'menu': ['EXPERIENCE', 'CONTACTS', 'SKILLS', 'EDUCATION', 'PROJECTS', 'LANGUAGES'],
+        'name': 'STROICH MARYIA',
+        'specialty': 'FRONT-END DEVELOPER',
+        'experience': [
+            'Current Project: Glizex.',
+            'I am actively involved in a startup where I apply my frontend development skills using technologies such as JavaScript, TypeScript, React, CSS, and HTML. My contribution helps shape a product that meets modern technological requirements and user needs.',
+            '💼 Corporate Website Development:',
+            'My experience also includes the successful revamping of the official organization website and the complete creation of a corporate website for a legal firm. These projects allowed me to showcase my frontend development skills and bring creative design to life while considering business objectives.'
+        ],
+        'contacts': [
+            'Batumi, Georgia','+995595077845','stroichmwork@gmail.com','stroich'
+        ],
+        'educationTitle':['University','Courses'],
+        'education': {
+            'university': 'Belarusian State Technological University',
+            'courses': [
+                'Website development with html, css and javascript (108 academic hours)',
+                'JAVASCRIPT/FRONT-END 2023Q1 (JAVASCRIPT)',
+                'React 2023 Q4'
+            ]
+        },
+        'projects':[
+            'Momentum',
+            'Virtual-Keyboard',
+            'Plants',
+            'RSS-CSS-Selector',
+            'Recipes',
+            'GraphiQL'
+        ],
+        'languages':[
+            'Russian - native speaker',
+            'English - A2+ (B1 in process...)'
+        ],
+        'footer':'2024 by Stroich Maryia'
+
+    },
+    'ru': {
+        'menu': ["ОПЫТ", "КОНТАКТЫ", "НАВЫКИ", "ОБРАЗОВАНИЕ", "ПРОЕКТЫ", "ЯЗЫКИ"],
+        'name': 'СТРОИЧ МАРИЯ',
+        'specialty': 'ФРОНТЕНД РАЗРАБОТЧИК',
+        'experience': [
+            "Текущий проект: Glizex.",
+            "Я активно участвую в стартапе, где применяю свои навыки front-end разработки с использованием таких технологий, как JavaScript, TypeScript, React, CSS и HTML. Мой вклад помогает создавать продукт, отвечающий современным технологическим требованиям и потребностям пользователей.",
+            " 💼 Разработка корпоративного веб-сайта:",
+            "Мой опыт также включает успешную реконструкцию официального веб-сайта организации и полное создание корпоративного веб-сайта для юридической фирмы. Эти проекты позволили мне продемонстрировать свои навыки разработки интерфейсов и воплотить креативный дизайн в жизнь с учетом бизнес-целей"
+        ],
+        'contacts': [
+            'Батуми, Грузия','+995595077845','stroichmwork@gmail.com','stroich'
+        ],
+        'educationTitle':["Университет",'Курсы'],
+        'education': {
+            'university': "Белорусский государственный технологический университет",
+            'courses': [
+                "Разработка веб-сайтов с использованием html, css и javascript (108 академических часов)",
+                "JAVASCRIPT/FRONT-END 2023Q1 (JAVASCRIPT)",
+                "React 2023 Q4"
+            ]
+        },
+        'projects':[
+            'Моментум',
+            'Виртуальная клавиатура',
+            'Растения',
+            'RSS-CSS-Selector',
+            'Рецепты',
+            'GraphiQL'
+        ],
+        'languages':[
+            "Русский - носитель языка",
+            "Английский - A2+ (B1 в процессе...)"
+        ],
+        'footer':'2024   Строич Мария'
+    },
+};
